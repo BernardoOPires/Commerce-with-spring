0a63eaee3c1fe249adb6d12757f83c6b40fbe95b
BernardoOPires <bernardoopires14@gmail.com>
2025-07-05 15:37:17 -0300

    201

--- DsCommerce/src/main/java/com/devsuperior/DsCommerce/services/ProductService.java
+++ DsCommerce/src/main/java/com/devsuperior/DsCommerce/services/ProductService.java
@@ -53,4 +53,16 @@ public class ProductService {
         Page<Product> result = repo.findAll(pageable);
         return result.map(x -> new ProductDTO(x));
     }
+
+     @Transactional
+    public ProductDTO Insert(ProductDTO dto){
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgURL(dto.getImgUrl());
+
+        entity = repo.save(entity);
+        return new ProductDTO(entity);
+    }
 }
