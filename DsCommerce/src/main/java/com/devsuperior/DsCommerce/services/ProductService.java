5bfee6184c8e99f1726b7b2baf0b227d5e81e7ce
BernardoOPires <bernardoopires14@gmail.com>
2025-07-05 13:15:15 -0300

    getProducts

--- DsCommerce/src/main/java/com/devsuperior/DsCommerce/services/ProductService.java
+++ DsCommerce/src/main/java/com/devsuperior/DsCommerce/services/ProductService.java
@@ -3,6 +3,8 @@ package com.devsuperior.DsCommerce.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,4 +35,22 @@ public class ProductService {
     //     Product product = repo.findById(id).get();
     //     return new ProductDTO(product);  
     // }
+
+    //retorna tudo
+    // @Transactional(readOnly = true)
+    // public List<ProductDTO> findAll(){
+    //     List<Product> result = repo.findAll();
+    //     return result.stream().map(x -> new ProductDTO(x)).toList();
+    // }
+
+    //paginado -- basta adicionar ?size="num de itens", http://localhost:8080/products?size=12
+    //http://localhost:8080/products?size=12&page=1  o &page faz ele começar na pagina definida
+    //http://localhost:8080/products?size=12&page=0&sort=name, esse sort=name vai ordenar pela variavel name
+    //http://localhost:8080/products?size=12&page=0&sort=name,desc, faz ser decrescente
+    //use para definir a pagina e tamanho que o usuario vai ver no front
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> result = repo.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
+    }
 }
